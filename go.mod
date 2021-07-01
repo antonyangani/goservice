5eef99fe45cac409fd72e3fc6c8ea82fe4084e38
antonyangani <theantonymars@gmail.com>
2021-07-01 11:49:17 +0300

    Created the server, servemux, graceful timeout etc

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/antonyangani/goservice
+
+go 1.15
--- main.go
+++ main.go
@@ -1,26 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/antonyangani/goservice/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
 
-		// this logs output to terminal
-		log.Println("Hello world")
-		d, err := ioutil.ReadAll(r.Body)
+	// creating a new server mux
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh) // works just like http.HandleFunc which took a http.ResponseWriter and http.Request as parameters
+	sm.Handle("/goodbye", gh)
+
+	// creating a server in go. Its a function that takes some parameters
+	// This will enable granular control over timeouts for different types of paylods, idle timeouts, keepalives etc
+
+	server := http.Server{
+		Addr:         ":3000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := server.ListenAndServe()
 		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadGateway)
-			return
+			l.Fatal(err)
 		}
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye world")
-	})
-	http.ListenAndServe(":3000", nil)
+	}()
+
+	// creating signal channels
+	// these channels will be waiting for kill signals from the os then initiate a shutdown procedure
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+	// Graceful shutdown: This allows server to finish a transaction before shutdown; this prevents kicking clients off the server amid transaction
+	timeoutContex, _ := context.WithTimeout(context.Background(), 30*time.Second) // allows handlers 30s to finish transactions being handled and shutoff the server
+	server.Shutdown(timeoutContex)
 }
